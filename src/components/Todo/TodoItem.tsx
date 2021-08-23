7aead5dfd8583cff6c36818bdbb96fe9041f5e93
cynthiaspence7827 <cynthiaspence7827@gmail.com>
2021-08-23 02:45:07 -0400

    complete tutorials

--- src/components/Todo/TodoItem.tsx
+++ src/components/Todo/TodoItem.tsx
@@ -1,24 +1,101 @@
-import React, { MouseEvent } from 'react';
-
-export type TodoItem = {
-  id: number,
-  title: string,
-  is_completed: boolean
-};
+import * as React from 'react';
+import { gql, useMutation } from "@apollo/client";
+import { GET_MY_TODOS } from './TodoPrivateList';
+import { 
+  GetMyTodosQuery,
+  RemoveTodoMutationFn,
+  Todos,
+  ToggleTodoMutation,
+  ToggleTodoMutationVariables
+} from '../../generated/graphql';
 
 interface TodoItemType {
   index: number,
-  todo: TodoItem
+  todo: Pick<Todos, "id" | "title" | "is_completed">
 };
+
+const TOGGLE_TODO = gql`
+  mutation toggleTodo ($id: Int!, $isCompleted: Boolean!) {
+    update_todos(where: {id: {_eq: $id}}, _set: {is_completed: $isCompleted}) {
+      affected_rows
+      returning {
+        id
+        title
+        is_completed
+      }
+    }
+  }
+`;
+
+const REMOVE_TODO = gql`
+  mutation removeTodo ($id: Int!) {
+    delete_todos(where: {id: {_eq: $id}}) {
+      affected_rows
+    }
+  }
+`;
 
 const TodoItem = ({index, todo}: TodoItemType) => {
 
-  const removeTodo = (e: MouseEvent) => {
+  const [todoUpdate] = useMutation<ToggleTodoMutation, ToggleTodoMutationVariables>(
+    TOGGLE_TODO, 
+    {
+      update(cache, { data }) {
+        const existingTodos = cache.readQuery<GetMyTodosQuery>({ query: GET_MY_TODOS });
+        const newTodos = existingTodos!.todos.map(t => {
+          if (t.id === todo.id) {
+            return { ...t, ...data!.update_todos!.returning[0] };
+          } else {
+            return t;
+          }
+        });
+        cache.writeQuery<GetMyTodosQuery>({
+          query: GET_MY_TODOS,
+          data: {todos: newTodos}
+        });
+      }
+    }
+  );
+
+  const [todoRemove] = useMutation<RemoveTodoMutationFn>(
+    REMOVE_TODO, 
+    {
+      update(cache, { data }) {
+        const existingTodos = cache.readQuery<GetMyTodosQuery>({ query: GET_MY_TODOS });
+        const newTodos = existingTodos!.todos.filter(t => (t.id !== todo.id));
+        cache.writeQuery<GetMyTodosQuery>({
+          query: GET_MY_TODOS,
+          data: {todos: newTodos}
+        });
+      }
+    }
+  );
+
+  const removeTodo = (e: React.MouseEvent) => {
     e.preventDefault();
     e.stopPropagation();
+    todoRemove({
+      variables: { id: todo.id },
+    });
   };
 
   const toggleTodo = () => {
+    todoUpdate({
+      variables: { id: todo.id, isCompleted: !todo.is_completed },
+      optimisticResponse: {
+        __typename: "mutation_root",
+        update_todos: {
+          __typename: "todos_mutation_response",
+          affected_rows: 1,
+          returning: [{
+            __typename: "todos",
+            id: todo.id,
+            title: todo.title,
+            is_completed: !todo.is_completed,
+          }]
+        }
+      }
+    });
   };
 
   return (
@@ -41,7 +118,7 @@ const TodoItem = ({index, todo}: TodoItemType) => {
         </div>
       </div>
 
-      <button className="closeBtn" onClick={(e) => removeTodo}>
+      <button className="closeBtn" onClick={(e) => removeTodo(e)}>
         x
       </button>
     </li>
--- src/components/Todo/TodoPrivateList.tsx
+++ src/components/Todo/TodoPrivateList.tsx
@@ -1,45 +1,66 @@
 import React, { Fragment, useState } from "react";
+import { gql, useMutation, useQuery } from "@apollo/client";
+
 import TodoItem from "./TodoItem";
 import TodoFilters from "./TodoFilters";
 
-type Todo = {
-  id: number,
-  title: string,
-  is_completed: boolean
-};
+import { 
+  GetMyTodosQuery,
+  ClearCompletedMutation,
+  Todos
+} from '../../generated/graphql';
+
+const GET_MY_TODOS = gql`
+  query getMyTodos {
+    todos(where: { is_public: { _eq: false} }, order_by: { created_at: desc }) {
+      id
+      title
+      is_completed
+    }
+  }
+`;
+
+const CLEAR_COMPLETED = gql`
+  mutation clearCompleted {
+    delete_todos(where: {is_completed: {_eq: true}, is_public: {_eq: false}}) {
+      affected_rows
+    }
+  }
+`;
 
 const TodoPrivateList = () => {
 
   const [filter, setFilter] = useState<string>("all");
-
-  const todos = [
-    {
-      id: 1,
-      title: "This is private todo 1",
-      is_completed: true
-    },
-    {
-      id: 2,
-      title: "This is private todo 2",
-      is_completed: false
-    }
-  ];
+  const { loading, error, data } = useQuery<GetMyTodosQuery>(GET_MY_TODOS);
+  const [clearTodos] = useMutation<ClearCompletedMutation>(
+      CLEAR_COMPLETED, 
+      {
+        update(cache, { data }) {
+          const existingTodos = cache.readQuery<GetMyTodosQuery>({ query: GET_MY_TODOS });
+          const newTodos = existingTodos!.todos.filter(t => (!t.is_completed));
+          cache.writeQuery<GetMyTodosQuery>({query:GET_MY_TODOS, data: {todos: newTodos}});
+        }
+      });
 
   const filterResults = (filter: string): void => {
     setFilter(filter);
   };
 
-  const clearCompleted = () => {
-  };
-
-  let filteredTodos = todos;
-  if (filter === "active") {
-    filteredTodos = todos.filter((todo: Todo) => todo.is_completed !== true);
-  } else if (filter === "completed") {
-    filteredTodos = todos.filter((todo: Todo) => todo.is_completed === true);
+  if(loading) {
+    return (<div>Loading...</div>);
+  }
+  if(error || !data) {
+    return (<div>Error...</div>);
   }
 
-  const todoList = filteredTodos.map((todo: Todo, index: number) => (
+  let filteredTodos = data.todos;
+  if (filter === "active") {
+    filteredTodos = data.todos.filter((todo: Pick<Todos, "id" | "title" | "is_completed">) => todo.is_completed !== true);
+  } else if (filter === "completed") {
+    filteredTodos = data.todos.filter((todo: Pick<Todos, "id" | "title" | "is_completed">) => todo.is_completed === true);
+  }
+
+  const todoList = filteredTodos.map((todo: Pick<Todos, "id" | "title" | "is_completed">, index: number) => (
     <TodoItem
       key={'item'+index}
       index={index}
@@ -59,10 +80,11 @@ const TodoPrivateList = () => {
         todos={filteredTodos}
         currentFilter={filter}
         filterResultsFn={filterResults}
-        clearCompletedFn={clearCompleted}
+        clearCompletedFn={clearTodos}
       />
     </Fragment>
   );
 }
 
 export default TodoPrivateList;
+export { GET_MY_TODOS };
